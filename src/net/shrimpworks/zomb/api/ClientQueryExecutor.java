e987f5269d0e74bf0d96d9f5fb0b418f505e0e6f
shrimpza <git@shrimpworks.za.net>
2014-11-30 19:15:57 +0200

    proceed with client api test. extend executor interface with canExecute(Plugin)

--- src/net/shrimpworks/zomb/api/ClientQueryExecutor.java
+++ src/net/shrimpworks/zomb/api/ClientQueryExecutor.java
@@ -2,8 +2,17 @@ package net.shrimpworks.zomb.api;
 
 import net.shrimpworks.zomb.entities.Query;
 import net.shrimpworks.zomb.entities.Response;
+import net.shrimpworks.zomb.entities.plugin.Plugin;
 
 public interface ClientQueryExecutor {
+
+	/**
+	 * Test whether this executor may execute queries for a specific plugin.
+	 *
+	 * @param plugin plugin to check
+	 * @return true if this executor can execute queries for this plugin
+	 */
+	public boolean canExecute(Plugin plugin);
 
 	/**
 	 * Execute a prepared query.
--- test/net/shrimpworks/zomb/api/ClientAPIServiceTest.java
+++ test/net/shrimpworks/zomb/api/ClientAPIServiceTest.java
@@ -7,16 +7,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.sun.net.httpserver.HttpServer;
+import net.shrimpworks.zomb.common.HttpClient;
 import net.shrimpworks.zomb.entities.Query;
 import net.shrimpworks.zomb.entities.Response;
 import net.shrimpworks.zomb.entities.application.ApplicationImpl;
 import net.shrimpworks.zomb.entities.application.ApplicationRegistry;
 import net.shrimpworks.zomb.entities.application.ApplicationRegistryImpl;
+import net.shrimpworks.zomb.entities.plugin.CommandRegistryImpl;
+import net.shrimpworks.zomb.entities.plugin.Plugin;
+import net.shrimpworks.zomb.entities.plugin.PluginImpl;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class ClientAPIServiceTest {
@@ -34,6 +40,7 @@ public class ClientAPIServiceTest {
 		Set<ClientQueryExecutor> executors = new HashSet<>();
 		executors.add(new PluginManagementExecutor());
 		executors.add(new HelpExecutor());
+		executors.add(new HttpExecutor());
 
 		this.service = new ClientAPIServiceImpl(host, port, appRegistry, executors);
 	}
@@ -44,10 +51,23 @@ public class ClientAPIServiceTest {
 	}
 
 	@Test
-	public void queryTest() {
+	public void queryTest() throws IOException {
 		this.appRegistry.add(new ApplicationImpl("client", "ckey", null, null));
+		this.appRegistry.find("client").plugins().add(new PluginImpl("plugin", "Provides plugin management functions",
+				new CommandRegistryImpl(), null, null));
+		this.appRegistry.find("client").plugins().add(new PluginImpl("help", "Provides plugin and command help functions",
+				new CommandRegistryImpl(), null, null));
 
-		fail("todo");
+		HttpClient client = new HttpClient(1000);
+
+		String pluginList = client.post(String.format("http://localhost:%d", port), "{" +
+				"  \"key\": \"ckey\"," +
+				"  \"user\": \"jane\"," +
+				"  \"query\": \"plugin list\"" +
+				"}");
+
+		assertTrue(pluginList.contains("plugin"));
+		assertTrue(pluginList.contains("help"));
 	}
 
 	public static class ClientAPIServiceImpl implements ClientAPIService {
@@ -79,12 +99,36 @@ public class ClientAPIServiceTest {
 	public static class PluginManagementExecutor implements ClientQueryExecutor {
 
 		@Override
+		public boolean canExecute(Plugin plugin) {
+			return plugin.name().equals("plugin");
+		}
+
+		@Override
 		public Response execute(Query query) {
 			throw new UnsupportedOperationException("Method not implemented.");
 		}
 	}
 
 	public static class HelpExecutor implements ClientQueryExecutor {
+
+		@Override
+		public boolean canExecute(Plugin plugin) {
+			return plugin.name().equals("help");
+		}
+
+		@Override
+		public Response execute(Query query) {
+			throw new UnsupportedOperationException("Method not implemented.");
+		}
+	}
+
+	public static class HttpExecutor implements ClientQueryExecutor {
+
+		@Override
+		public boolean canExecute(Plugin plugin) {
+			return plugin.url() != null && plugin.url().matches("http://.+");
+		}
+
 		@Override
 		public Response execute(Query query) {
 			throw new UnsupportedOperationException("Method not implemented.");
