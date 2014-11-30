6d79ef3c4e3d541c95e444dacc5a59ca67f63eea
shrimpza <git@shrimpworks.za.net>
2014-11-30 15:44:50 +0200

    starting test for an http client

---
+++ test/net/shrimpworks/zomb/common/HttpClientTest.java
@@ -0,0 +1,13 @@
+package net.shrimpworks.zomb.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class HttpClientTest {
+
+	@Test
+	public void httpClientTest() {
+		fail("todo");
+	}
+}
