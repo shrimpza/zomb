6023e5e8a0390cdef17d0126aa32842afb84c808
shrimpza <git@shrimpworks.za.net>
2014-12-01 13:28:11 +0200

    move help into it's own source file

--- test/net/shrimpworks/zomb/api/ClientAPIServiceTest.java
+++ test/net/shrimpworks/zomb/api/ClientAPIServiceTest.java
@@ -2,8 +2,6 @@ package net.shrimpworks.zomb.api;
 
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 import com.eclipsesource.json.JsonArray;
@@ -11,17 +9,11 @@ import com.eclipsesource.json.JsonObject;
 import net.jadler.Jadler;
 import net.shrimpworks.zomb.common.HttpClient;
 import net.shrimpworks.zomb.entities.Query;
-import net.shrimpworks.zomb.entities.QueryImpl;
 import net.shrimpworks.zomb.entities.Response;
-import net.shrimpworks.zomb.entities.ResponseImpl;
 import net.shrimpworks.zomb.entities.application.ApplicationImpl;
 import net.shrimpworks.zomb.entities.application.ApplicationRegistry;
 import net.shrimpworks.zomb.entities.application.ApplicationRegistryImpl;
-import net.shrimpworks.zomb.entities.plugin.Command;
-import net.shrimpworks.zomb.entities.plugin.CommandImpl;
-import net.shrimpworks.zomb.entities.plugin.CommandRegistryImpl;
 import net.shrimpworks.zomb.entities.plugin.Plugin;
-import net.shrimpworks.zomb.entities.plugin.PluginImpl;
 
 import org.junit.After;
 import org.junit.Before;
@@ -267,67 +259,6 @@ public class ClientAPIServiceTest {
 		assertEquals("help", json.get("plugin").asString());
 		assertTrue(json.get("response").asArray().get(0).asString().equals("list installed plugins"));
 	}
-
-
-	public static class Help extends PluginImpl {
-
-		public Help() {
-			super("help", "Provides plugin and command help functions", new CommandRegistryImpl(), null, null);
-			commands().add(new CommandImpl("show", "shows help for a plugin or plugin command", 0, "[A-Za-z]+|[A-Za-z]+ [A-Za-z]+"));
-			commands().add(new CommandImpl("list", "list commands available in a plugin", 1, null));
-		}
-
-		public static class Executor implements ClientQueryExecutor {
-
-			@Override
-			public boolean canExecute(Plugin plugin) {
-				return plugin instanceof Help;
-			}
-
-			@Override
-			public Response execute(Query query) {
-				switch (query.command().name()) {
-					case "show":
-						return show(query);
-					case "list":
-						return list(query);
-					default:
-						throw new IllegalArgumentException("Unknown plugin command");
-				}
-			}
-
-			private Response show(Query query) {
-				List<String> args = QueryImpl.parseQuery(query.args().get(0));
-
-				Plugin plugin = query.application().plugins().find(args.get(0));
-
-				String help;
-				if (args.size() > 1) {
-					help = plugin.commands().find(args.get(1)).help();
-				} else {
-					help = plugin.help();
-				}
-
-				return new ResponseImpl(query, new String[]{help}, null);
-			}
-
-			private Response list(Query query) {
-				Plugin plugin = query.application().plugins().find(query.args().get(0));
-
-				StringBuilder sb = new StringBuilder();
-				Iterator<Command> commands = plugin.commands().all().iterator();
-				while (commands.hasNext()) {
-					sb.append(commands.next().name());
-					if (commands.hasNext()) {
-						sb.append(", ");
-					}
-				}
-
-				return new ResponseImpl(query, new String[]{sb.toString()}, null);
-			}
-		}
-	}
-
 
 	public static class HttpExecutor implements ClientQueryExecutor {
 
