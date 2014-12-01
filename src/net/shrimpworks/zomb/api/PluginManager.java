b78270c41924da99eb9918210c2c23d373b14a77
shrimpza <git@shrimpworks.za.net>
2014-12-01 11:39:02 +0200

    move plugin manager and plugin manager executor into their own class, with the executor inside the manager (one's useless without the other)

---
+++ src/net/shrimpworks/zomb/api/PluginManager.java
@@ -0,0 +1,125 @@
+package net.shrimpworks.zomb.api;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import net.shrimpworks.zomb.common.HttpClient;
+import net.shrimpworks.zomb.entities.Query;
+import net.shrimpworks.zomb.entities.Response;
+import net.shrimpworks.zomb.entities.ResponseImpl;
+import net.shrimpworks.zomb.entities.plugin.CommandImpl;
+import net.shrimpworks.zomb.entities.plugin.CommandRegistryImpl;
+import net.shrimpworks.zomb.entities.plugin.Plugin;
+import net.shrimpworks.zomb.entities.plugin.PluginImpl;
+
+public class PluginManager extends PluginImpl {
+
+	public PluginManager() {
+		super("plugin", "Provides plugin management functionality", new CommandRegistryImpl(), null, null);
+		commands().add(new CommandImpl("list", "list installed plugins", 0, null));
+		commands().add(new CommandImpl("add", "add a new plugin", 1, null));
+		commands().add(new CommandImpl("remove", "remove an installed plugin", 1, null));
+	}
+
+	public static class Executor implements ClientQueryExecutor {
+
+		private static final Logger logger = Logger.getLogger(Executor.class.getName());
+
+		private final HttpClient client;
+
+		public Executor() {
+			this.client = new HttpClient(5000); // TODO config
+		}
+
+		@Override
+		public boolean canExecute(Plugin plugin) {
+			return plugin instanceof PluginManager;
+		}
+
+		@Override
+		public Response execute(Query query) {
+			switch (query.command().name()) {
+				case "list":
+					return list(query);
+				case "add":
+					return add(query);
+				case "remove":
+					return remove(query);
+				default:
+					throw new IllegalArgumentException("Unknown plugin command");
+			}
+		}
+
+		private Response list(Query query) {
+			StringBuilder sb = new StringBuilder();
+			Iterator<Plugin> plugins = query.application().plugins().all().iterator();
+			while (plugins.hasNext()) {
+				sb.append(plugins.next().name());
+				if (plugins.hasNext()) {
+					sb.append(", ");
+				}
+			}
+
+			return new ResponseImpl(query.plugin(), query.user(), query.query(), new String[]{sb.toString()}, null);
+		}
+
+		private Response add(Query query) {
+			try {
+				String pluginDefSource = client.get(query.args().get(0));
+
+				JsonObject pluginDef = JsonObject.readFrom(pluginDefSource);
+
+				if (query.application().plugins().find(pluginDef.get("plugin").asString()) != null) {
+					return new ResponseImpl(query, new String[]{"Failed to add plugin: already exists"}, null);
+				}
+
+				if (pluginDef.get("commands").asArray().isEmpty()) {
+					return new ResponseImpl(query, new String[]{"Failed to add plugin: no commands"}, null);
+				}
+
+				Plugin plugin = new PluginImpl(
+						pluginDef.get("plugin").asString(),
+						pluginDef.get("help").asString(),
+						new CommandRegistryImpl(),
+						query.args().get(0),
+						pluginDef.get("contact").asString());
+
+				for (JsonValue commands : pluginDef.get("commands").asArray()) {
+					plugin.commands().add(new CommandImpl(
+							commands.asObject().get("command").asString(),
+							commands.asObject().get("help").asString(),
+							commands.asObject().get("args").asInt(),
+							commands.asObject().get("pattern").asString()
+					));
+				}
+
+				if (query.application().plugins().add(plugin)) {
+					return new ResponseImpl(query, new String[]{String.format("Success, added new plugin %s", plugin.name())}, null);
+				} else {
+					return new ResponseImpl(query, new String[]{"Failed to add plugin to application"}, null);
+				}
+			} catch (IOException e) {
+				logger.log(Level.WARNING, "Failed to get plugin definition", e);
+
+				return new ResponseImpl(query, new String[]{"Failed to get plugin: " + e.getMessage()}, null);
+			}
+		}
+
+		private Response remove(Query query) {
+			Plugin plugin = query.application().plugins().find(query.args().get(0));
+			if (plugin != null) {
+				if (plugin == query.application().plugins().remove(plugin)) {
+					return new ResponseImpl(query, new String[]{"Success, removed plugin"}, null);
+				} else {
+					return new ResponseImpl(query, new String[]{"Failed to remove plugin from application"}, null);
+				}
+			} else {
+				return new ResponseImpl(query, new String[]{"Failed to remove plugin, does not exist"}, null);
+			}
+		}
+	}
+}
--- test/net/shrimpworks/zomb/api/ClientAPIServiceTest.java
+++ test/net/shrimpworks/zomb/api/ClientAPIServiceTest.java
@@ -4,12 +4,9 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
-import com.eclipsesource.json.JsonValue;
 import net.jadler.Jadler;
 import net.shrimpworks.zomb.common.HttpClient;
 import net.shrimpworks.zomb.entities.Query;
@@ -53,7 +50,7 @@ public class ClientAPIServiceTest {
 		this.appRegistry.find("client").plugins().add(new Help());
 
 		Set<ClientQueryExecutor> executors = new HashSet<>();
-		executors.add(new PluginManagementExecutor());
+		executors.add(new PluginManager.Executor());
 		executors.add(new HelpExecutor());
 		executors.add(new HttpExecutor());
 
@@ -252,113 +249,6 @@ public class ClientAPIServiceTest {
 		assertTrue(json.get("response").asArray().get(0).asString().contains("remove"));
 	}
 
-	public static class PluginManager extends PluginImpl {
-
-		public PluginManager() {
-			super("plugin", "Provides plugin management functionality", new CommandRegistryImpl(), null, null);
-			commands().add(new CommandImpl("list", "list installed plugins", 0, null));
-			commands().add(new CommandImpl("add", "add a new plugin", 1, null));
-			commands().add(new CommandImpl("remove", "remove an installed plugin", 1, null));
-		}
-	}
-
-	public static class PluginManagementExecutor implements ClientQueryExecutor {
-
-		private static final Logger logger = Logger.getLogger(PluginManagementExecutor.class.getName());
-
-		private final HttpClient client;
-
-		public PluginManagementExecutor() {
-			this.client = new HttpClient(5000); // TODO config
-		}
-
-		@Override
-		public boolean canExecute(Plugin plugin) {
-			return plugin instanceof PluginManager;
-		}
-
-		@Override
-		public Response execute(Query query) {
-			switch (query.command().name()) {
-				case "list":
-					return list(query);
-				case "add":
-					return add(query);
-				case "remove":
-					return remove(query);
-				default:
-					throw new IllegalArgumentException("Unknown plugin command");
-			}
-		}
-
-		private Response list(Query query) {
-			StringBuilder sb = new StringBuilder();
-			Iterator<Plugin> plugins = query.application().plugins().all().iterator();
-			while (plugins.hasNext()) {
-				sb.append(plugins.next().name());
-				if (plugins.hasNext()) {
-					sb.append(", ");
-				}
-			}
-
-			return new ResponseImpl(query.plugin(), query.user(), query.query(), new String[]{sb.toString()}, null);
-		}
-
-		private Response add(Query query) {
-			try {
-				String pluginDefSource = client.get(query.args().get(0));
-
-				JsonObject pluginDef = JsonObject.readFrom(pluginDefSource);
-
-				if (query.application().plugins().find(pluginDef.get("plugin").asString()) != null) {
-					return new ResponseImpl(query, new String[]{"Failed to add plugin: already exists"}, null);
-				}
-
-				if (pluginDef.get("commands").asArray().isEmpty()) {
-					return new ResponseImpl(query, new String[]{"Failed to add plugin: no commands"}, null);
-				}
-
-				Plugin plugin = new PluginImpl(
-						pluginDef.get("plugin").asString(),
-						pluginDef.get("help").asString(),
-						new CommandRegistryImpl(),
-						query.args().get(0),
-						pluginDef.get("contact").asString());
-
-				for (JsonValue commands : pluginDef.get("commands").asArray()) {
-					plugin.commands().add(new CommandImpl(
-							commands.asObject().get("command").asString(),
-							commands.asObject().get("help").asString(),
-							commands.asObject().get("args").asInt(),
-							commands.asObject().get("pattern").asString()
-					));
-				}
-
-				if (query.application().plugins().add(plugin)) {
-					return new ResponseImpl(query, new String[]{String.format("Success, added new plugin %s", plugin.name())}, null);
-				} else {
-					return new ResponseImpl(query, new String[]{"Failed to add plugin to application"}, null);
-				}
-			} catch (IOException e) {
-				logger.log(Level.WARNING, "Failed to get plugin definition", e);
-
-				return new ResponseImpl(query, new String[]{"Failed to get plugin: " + e.getMessage()}, null);
-			}
-		}
-
-		private Response remove(Query query) {
-			Plugin plugin = query.application().plugins().find(query.args().get(0));
-			if (plugin != null) {
-				if (plugin == query.application().plugins().remove(plugin)) {
-					return new ResponseImpl(query, new String[]{"Success, removed plugin"}, null);
-				} else {
-					return new ResponseImpl(query, new String[]{"Failed to remove plugin from application"}, null);
-				}
-			} else {
-				return new ResponseImpl(query, new String[]{"Failed to remove plugin, does not exist"}, null);
-			}
-		}
-	}
 
 	public static class Help extends PluginImpl {
 
