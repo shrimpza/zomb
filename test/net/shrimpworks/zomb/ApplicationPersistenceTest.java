d2df91acc53c6036bca30f99e77e4171f9e43114
shrimpza <git@shrimpworks.za.net>
2014-12-01 15:10:10 +0200

    application persistence test mostly in place.

--- test/net/shrimpworks/zomb/ApplicationPersistenceTest.java
+++ test/net/shrimpworks/zomb/ApplicationPersistenceTest.java
@@ -1,18 +1,46 @@
 package net.shrimpworks.zomb;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import net.shrimpworks.zomb.entities.application.Application;
+import net.shrimpworks.zomb.entities.application.ApplicationImpl;
+import net.shrimpworks.zomb.entities.plugin.CommandImpl;
+import net.shrimpworks.zomb.entities.plugin.CommandRegistryImpl;
+import net.shrimpworks.zomb.entities.plugin.PluginImpl;
+import net.shrimpworks.zomb.entities.user.UserImpl;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 public class ApplicationPersistenceTest {
 
 	@Test
 	public void applicationPersistenceTest() {
-		fail("todo");
+		Application application = new ApplicationImpl("app", "key", "http://url.com", "bob <bob@mail");
+
+		application.users().add(new UserImpl("bob"));
+		application.users().add(new UserImpl("jane"));
+
+		application.plugins().add(new PluginImpl("weather", "weather info", new CommandRegistryImpl(), "http://plugin.url", "joe@mail"));
+		application.plugins().find("weather").commands().add(new CommandImpl("current", "current weather", 1, ""));
+		application.plugins().find("weather").commands().add(new CommandImpl("tomorrow", "tomorrow's weather", 0, null));
+
+		application.plugins().add(new PluginImpl("math", "math ops", new CommandRegistryImpl(), "http://math.url", "sue@mail"));
+		application.plugins().find("math").commands().add(new CommandImpl("add", "add numbers", 0, ""));
+
+		ApplicationPersistence persistence = new ApplicationPersistence();
+		persistence.save(application);
+
+		List<Application> all = new ArrayList<>(persistence.all());
+
+		assertEquals(1, all.size());
+		assertEquals(application.name(), all.get(0).name());
+		assertEquals(application.key(), all.get(0).key());
+		assertEquals(application.url(), all.get(0).url());
+		assertEquals(application.contact(), all.get(0).contact());
 	}
 
 	public static class ApplicationPersistence implements Persistence<Application> {
