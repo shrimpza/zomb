7deee32d6223262acb5f87393b38e8fd9a52bee0
shrimpza <git@shrimpworks.za.net>
2014-12-06 21:11:40 +0200

    javadoc updates

--- src/net/shrimpworks/zomb/entities/Persistence.java
+++ src/net/shrimpworks/zomb/entities/Persistence.java
@@ -15,6 +15,7 @@ public interface Persistence<T> {
 	 *
 	 * @param entity entity to be persisted
 	 * @return true on success
+	 * @throws IOException something went wrong
 	 */
 	public boolean save(T entity) throws IOException;
 
@@ -23,6 +24,7 @@ public interface Persistence<T> {
 	 *
 	 * @param entity entity to be deleted
 	 * @return true on success
+	 * @throws IOException something went wrong
 	 */
 	public boolean delete(T entity) throws IOException;
 
@@ -30,6 +32,7 @@ public interface Persistence<T> {
 	 * Load all persisted entities
 	 *
 	 * @return all entities
+	 * @throws IOException something went wrong
 	 */
 	public Collection<T> all() throws IOException;
 }
