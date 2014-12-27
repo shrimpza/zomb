ed1ac47f2bb2d9d3d75b0602b1083f6de9298dc0
shrimpza <git@shrimpworks.za.net>
2014-12-27 07:29:08 +0200

    plan test for app api

---
+++ test/net/shrimpworks/zomb/api/ApplicationAPITest.java
@@ -0,0 +1,25 @@
+package net.shrimpworks.zomb.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ApplicationAPITest {
+
+	@Test
+	public void testAppAPI() {
+		// TODO add an application, verify result
+
+		// TODO query application by key
+
+		// TODO query application by name
+
+		// TODO request all applications
+
+		// TODO delete application
+
+		// TODO request all applications, ensuring deleted is gone
+
+		fail("todo");
+	}
+}
